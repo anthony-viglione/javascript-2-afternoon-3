f313a7bb37283cb33b7218ba5d9baca143f8591d
Anthony Viglione <viglione2014.2015@gmail.com>
2019-01-30 14:08:22 -0700

    Finished Required Problems

--- practice.js
+++ practice.js
@@ -28,7 +28,9 @@
 */
 
 // Code Here 
-
+function first(arr, cb){
+  cb(arr[0])
+}
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
@@ -48,7 +50,9 @@ first(names, function(firstName){
 */
 
 //Code Here
-
+function last(arr,cb){
+  cb(arr[arr.length-1])
+}
 // Do not edit the code below.
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -66,7 +70,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1,num2,cb){
+  cb(num1*num2)
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,7 +91,14 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
-
+function contains(arr, name, cb){
+  var numOfName=0
+  for(let i=0;i<arr.length;i++){
+    if (arr[i] === name) {numOfName++}
+  }
+  if (numOfName > 0){cb(true)}
+  else {cb(false)}
+}
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -107,6 +120,42 @@ contains(names, 'Colt', function(result){
 
 //Code Here
 
+
+
+
+
+
+
+// function uniq(arr,cb){
+  // let numDupes=-1
+  // for(var i=0;i<arr.length;i++){
+  //   // var val1=arr[i]
+  //   for(var j=0;j<arr.length;j++){
+  //     if (arr[i]===arr[j]){
+  //       numDupes++
+  //       if(numDupes>0){
+  //         arr.splice(arr[j],1)
+  //         numDupes--
+  //         j--
+  //       }
+  //     }
+  //   }
+  // }
+//   cb(arr)
+// }
+
+
+function uniq(arr,cb){
+  for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[i]===arr[j]){
+        arr.splice(j,1)
+        j--
+      }
+    }
+  }
+  cb(arr)
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
